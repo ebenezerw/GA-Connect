60b236f1965318b358d85e54add7ce9ea47609ff
KEVIN KELLER <keller_kevin@bah.com>
2017-03-13 12:01:14 -0400

    fixing show feature on index page

--- js/app.js
+++ js/app.js
@@ -5,9 +5,9 @@ angular
   .config(["$stateProvider", RouterFunction])
   .factory("GaFactory", ["$resource", GaFactoryFuntion])
   .controller("GaIndexController", ["GaFactory", GaIndexControllerFunction])
-  .controller("GaShowController", ["$stateParams", GaShowControllerFunction])
-  .controller("GaEditController", ["$stateParams", "$state", GaEditControllerFunction])
-  .controller("GaNewController", ["$state", GaNewControllerFunction])
+  .controller("GaShowController", ["GaFactory", "$stateParams", GaShowControllerFunction])
+  .controller("GaEditController", ["GaFactory", "$stateParams", "$state", GaEditControllerFunction])
+  .controller("GaNewController", ["GaFactory", "$state", GaNewControllerFunction])
 
 function RouterFunction($stateProvider) {
   $stateProvider
@@ -39,32 +39,32 @@ function RouterFunction($stateProvider) {
 
 function GaFactoryFuntion($resource) {
   // return json file link right here
-  return $resource("https://gist.githubusercontent.com/rodneycurl/df6c512078a645a764a13856aa1c155a/raw/c42459357d12395d6a61942791eb1d3c764625b7/ga-connect-seed-data.json")
+  return $resource( "https://gist.githubusercontent.com/rodneycurl/df6c512078a645a764a13856aa1c155a/raw/104b46ade7f5d68f74f9b4c747dce33e9569c2c7/ga-connect-seed-data.json" )
 }
 
-function GaIndexControllerFunction(GaFactory) {
-  this.GaFactory = GaFactory.get()
+function GaIndexControllerFunction( GaFactory ) {
+  this.users = GaFactory.query()
   // .query() expects to return an array
   // .get() expects to return an object
 }
 
-function GaShowControllerFunction($stateParams) {
-  this.GaFactory = GaFactory.get({id: $stateParams.id})
+function GaShowControllerFunction( GaFactory, $stateParams ) {
+  this.user = GaFactory.get({id: $stateParams.id})
 }
 
 function GaEditControllerFunction($stateParams, $state) {
-  this.GaFactory = GaFactory.get({id: $stateParams.id})
+  this.user = GaFactory.get({id: $stateParams.id})
   this.update = function() {
-    this.GaFactory.$update({id: $stateParams.id})
+    this.user.$update({id: $stateParams.id})
   }
   this.destroy = function() {
-    this.GaFactory.$delete({id: $stateParams.id})
+    this.user.$delete({id: $stateParams.id})
   }
 }
 
-function GaNewControllerFunction($state) {
-  this.GaFactory = new GaFactory()
+function GaNewControllerFunction(GaFactory, $state) {
+  this.user = new GaFactory()
   this.create = function() {
-    this.GaFactory.$save()
+    this.user.$save()
   }
 }
