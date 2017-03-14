bd81d7317ee4923fde50e5436a95bf9322e7b1ba
KEVIN KELLER <keller_kevin@bah.com>
2017-03-14 12:56:35 -0400

    new page shows properly

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
 "use strict";
-
 angular
   .module("gaConnect", ["ui.router", "ngResource"])
   .config(["$stateProvider", RouterFunction])
-  .factory("GaFactory", ["$resource", GaFactoryFunction])
+  .factory("GaFactory", ["$resource", GaFactoryFuntion])
   .controller("GaIndexController", ["GaFactory", GaIndexControllerFunction])
   .controller("GaShowController", ["GaFactory", "$stateParams", GaShowControllerFunction])
   .controller("GaEditController", ["GaFactory", "$stateParams", "$state", GaEditControllerFunction])
-  .controller("GaNewController", ["GaFactory", "$state", GaNewControllerFunction])
-
+  .controller("GaNewController", ["GaFactory", "$state", GaNewControllerFunction]);
 function RouterFunction($stateProvider) {
   $stateProvider
   .state("gaIndex", {
@@ -18,44 +16,45 @@ function RouterFunction($stateProvider) {
     controllerAs: "vm"
   })
   .state("gaShow", {
-    url: "/gaconnect/:id",
+    url: "gaconnect/:id",
     templateUrl: "js/ng-views/show.html",
     controller: "GaShowController",
-
+    controllerAs: "vm"
   })
   .state("gaEdit", {
-    url: "/gaconnect/:id/edit",
+    url: "gaconnect/:id/edit",
     templateUrl: "js/ng-views/edit.html",
     controller: "GaEditController",
     controllerAs: "vm"
   })
   .state("gaNew", {
-    url: "/gaconnect/new",
+    url: "gaconnect/new",
     templateUrl: "js/ng-views/new.html",
     controller: "GaNewController",
     controllerAs: "vm"
   })
 }
-
-function GaFactoryFunction($resource, $stateProvider) {
+function GaFactoryFuntion($resource) {
   // return json file link right here
-  return $resource( "https://gist.githubusercontent.com/rodneycurl/df6c512078a645a764a13856aa1c155a/raw/58524dd4e5f25e100f5db4463f97a722b1b090c1/ga-connect-seed-data.json" )
+  return $resource( "https://gist.githubusercontent.com/rodneycurl/df6c512078a645a764a13856aa1c155a/raw/58524dd4e5f25e100f5db4463f97a722b1b090c1/ga-connect-seed-data.json", {}, {
+    update: {method: "PUT"}
+  })
   // return $resource("http://localhost:3000/users/:id", {}, {
-  //   update: {
+  // update: {
   //     method: "PUT"
   //   }
-  // })
+  //  })
 }
-var vm = this;
-function GaIndexControllerFunction( GaFactory) {
-  // var vm = this;
+
+function GaIndexControllerFunction( GaFactory ) {
   this.users = GaFactory.query()
   // .query() expects to return an array
   // .get() expects to return an object
 }
 
-function GaShowControllerFunction( GaFactory, $stateParams ) {
-  this.user = GaFactory.get({id: $stateParams.id})
+function GaShowControllerFunction( GaFactory, $stateParams, $state ) {
+  var vm = this;
+  vm.user = GaFactory.get({id: $stateParams.id});
 }
 
 function GaEditControllerFunction($stateParams, $state) {
